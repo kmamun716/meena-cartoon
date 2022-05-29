5014abdf5672743a584b6de72d72866af2d7eeda
Kazi Mamun <kazimamun716@gmail.com>
2022-05-29 19:56:48 +0600

    site created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Dashboard from './components/Dashboard/Dashboard';
+import Home from './components/Home';
+import NotFound from './components/NotFound/NotFound';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/reviews' element={<Reviews />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/blog' element={<Blogs />} />
+        <Route path='/about-us' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
