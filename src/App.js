8c7462064a3d44fc979c0efc3dad17b6ba1f214e
Kazi Mamun <kazimamun716@gmail.com>
2022-05-29 21:18:42 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Home from './components/Home';
 import NotFound from './components/NotFound/NotFound';
 import Reviews from './components/Reviews/Reviews';
+import Header from './components/shared/Header/Header';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container mt-2">
+      <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/reviews' element={<Reviews />} />
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Nav variant="tabs" sticky="top" fixed="top" defaultActiveKey="/">
+            <Nav.Item>
+                <LinkContainer to='/'>
+                    <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+                <LinkContainer to='/reviews'>
+                    <Nav.Link>Reviews</Nav.Link>
+                </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+                <LinkContainer to='/blog'>
+                    <Nav.Link>Blogs</Nav.Link>
+                </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+                <LinkContainer to='/about-us'>
+                    <Nav.Link>About</Nav.Link>
+                </LinkContainer>
+            </Nav.Item>
+        </Nav>
     );
 };
 
